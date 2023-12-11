1aa6a0892b733c25df256f543422037aa5dde713
norah30 <105987440+norah30@users.noreply.github.com>
2023-12-10 19:34:31 -0500

    Update clientes.go

--- clientes.go
+++ clientes.go
@@ -20,43 +20,53 @@ type Cliente struct {
 	Celular   string
 	Eliminado bool
 }
+// definimos el metodo mostrar clientes, recibe una conexion a la base de datos y devuelve un error si hay un problema.
+func (c *Cliente) MostrarCliente(b *base.Base, args ...interface{}) {
+	query := "SELECT CedulaCli, CodigoLib, Nombres, Apellidos, Correo, Direccion, NumeroTar, Targeta, CodigoSeg, Celular, Eliminado FROM clientes WHERE CedulaCli= ?;"
+	rows, err := b.DB.Query(query, args...)
+	if err != nil {
+		fmt.Errorf("error al consultar cliente: %w", err)
+	}
+	defer rows.Close()
 
-// definimos el metodo mostrar clientes
-func (c *Cliente) MostrarCliente() {
-	fmt.Println("Cédula: ", c.CedulaCli)
-	fmt.Println("Nombres: ", c.Nombres)
-	fmt.Println("Apellidos: ", c.Apellidos)
-	fmt.Println("Dirección: ", c.Direccion)
-	fmt.Println("Celular: ", c.Celular)
-	fmt.Println("Correo: ", c.Correo)
-	fmt.Println("Código de libro: ", c.CodigoLib)
-	fmt.Println("Tarjeta: ", c.Tarjeta)
-	fmt.Println("Número de tarjeta: ", c.NumeroTar)
-	fmt.Println("Código de seguridad: ", c.CodigoSeg)
+	for rows.Next() {
+		var cliente Cliente
+		err := rows.Scan(&cliente.CedulaCli, &cliente.CodigoLib, &cliente.Nombres, &cliente.Apellidos, &cliente.Correo, &cliente.Direccion, &cliente.NumeroTar, &cliente.Tarjeta, &cliente.CodigoSeg, &cliente.Celular, &cliente.Eliminado)
+		if err != nil {
+			fmt.Errorf("error al mostrar: %w", err)
+		}
+		fmt.Println(cliente)
+	}
+	defer basedatos.CerrarConexion(b.DB)
 }
 
-// definimos el metodo editar clientes
-func (c *Cliente) EditarCliente(nuevaCedulaCli string, nuevaCodigoLib int, nuevoNombres string, nuevoApellidos string, nuevoCorreo string, nuevaDireccion string, nuevoNumeroTar string, nuevaTarjeta string, nuevoCodigoSeg int, nuevoCelular string) {
-	c.CedulaCli = nuevaCedulaCli
-	c.CodigoLib = nuevaCodigoLib
-	c.Nombres = nuevoNombres
-	c.Apellidos = nuevoApellidos
-	c.Correo = nuevoCorreo
-	c.Direccion = nuevaDireccion
-	c.NumeroTar = nuevoNumeroTar
-	c.Tarjeta = nuevaTarjeta
-	c.CodigoSeg = nuevoCodigoSeg
-	c.Celular = nuevoCelular
+// definimos el metodo editar clientes, recibe una conexion a la base de datos y devuelve un error si hay un problema con la actualizacion de la base.
+func (c *Cliente) EditarCliente(b *base.Base, nuevo Cliente) error {
+	query := "UPDATE clientes SET CedulaCli= ?, CodigoLib= ?, Nombres= ?, Apellidos= ?, Correo= ?, Direccion= ?, NumeroTar= ?, Targeta= ?, CodigoSeg= ?, Celular= ?, Eliminado= ? WHERE CedulaCli= ?; "
+	_, err := b.DB.Exec(query, nuevo.CedulaCli, nuevo.CodigoLib, nuevo.Nombres, nuevo.Apellidos, nuevo.Correo, nuevo.Direccion, nuevo.NumeroTar, nuevo.Tarjeta, nuevo.CodigoSeg, nuevo.Celular, nuevo.Eliminado, c.CedulaCli)
+	if err != nil {
+		return fmt.Errorf("error al editar cliente: %w", err)
+	}
+	return nil
 }
 
-// definimos el metodo eliminar cliente
-func (c *Cliente) EliminarCliente() {
-	c.Eliminado = true
+// definimos el metodo eliminar cliente, recibe una conexion a la base de datos y devuelve un error si hay un problema con la eliminacion del dato.
+func (c *Cliente) EliminarCliente(b *base.Base) error {
+	query := `DELETE FROM clientes WHERE CedulaCli = ?`
+	_, err := b.DB.Exec(query, c.CedulaCli)
+	if err != nil {
+		fmt.Errorf("error al eliminar cliente: %w", err)
+	}
+	return nil
 }
 
-// definimos el metodo insertar cliente
-func (c *Cliente) InsertarCliente(db *sql.DB) error {
-	query := `INSERT INTO clientes (cedula_cli, codigo_lib, nombres, apellidos, correo, direccion, numero_tar, tarjeta, codigo_seg, celular) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
-	_, err := db.Exec(query, c.CedulaCli, c.CodigoLib, c.Nombres, c.Apellidos, c.Correo, c.Direccion, c.NumeroTar, c.Tarjeta, c.CodigoSeg, c.Celular)
-	return err
+// definimos el metodo insertar cliente, recibe una conexion a la base de datos y devuelve un error si hay un problema con insertar datos en  la base.
+func (c *Cliente) InsertarCliente(b *base.Base) error {
+	query := `INSERT INTO clientes (CedulaCli, CodigoLib, Nombres, Apellidos, Correo, Direccion, NumeroTar, Tarjeta, CodigoSeg, Celular, Eliminado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)`
+	_, err := b.DB.Exec(query, c.CedulaCli, c.CodigoLib, c.Nombres, c.Apellidos, c.Correo, c.Direccion, c.NumeroTar, c.Tarjeta, c.CodigoSeg, c.Celular)
+	if err != nil {
+		return fmt.Errorf("error al insertar cliente: %w", err)
+	}
+	return nil
 }
+
